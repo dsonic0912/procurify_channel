042834d1d80d97f0afc52cd5c6ac6ea61be97b7a
David Tung <david@lbstek.com>
2016-08-19 11:12:25 -0700

    test out bootstrap-react

--- assets/js/App.jsx
+++ assets/js/App.jsx
@@ -1,12 +1,31 @@
+require('../css/lib/bootstrap/css/bootstrap.min.css');
+require('../css/dashboard.css');
+
 import React, {Component} from 'react';
-import { Button } from 'react-bootstrap';
+import { Grid,Row,Col } from 'react-bootstrap';
+import { Nav, NavItem } from 'react-bootstrap';
+import { FormControl } from 'react-bootstrap';
 
 class App extends Component {
 	render() {
 		return (
-			<div>
-				<h1>Hello Procurify</h1>
-			</div>
+			<Row>
+				<Col md={2}>
+					<h1>Left Col</h1>
+				</Col>
+				<Col md={10} className="main">
+					<Row>
+						<Col md={12}>
+							<h1>Main Content</h1>
+						</Col>
+					</Row>
+					<Row>
+						<Col md={12}>
+							<FormControl componentClass="textarea" />
+						</Col>
+					</Row>
+				</Col>
+			</Row>
 		);
 	}
 }
